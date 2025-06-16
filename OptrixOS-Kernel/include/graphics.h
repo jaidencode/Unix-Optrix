8c04f883479dee02e8d8b103cfc10554a7d958b4
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-15 20:19:03 -0400

    Replace kernel with minimal graphics boot

--- OptrixOS-Kernel/include/graphics.h
+++ OptrixOS-Kernel/include/graphics.h
@@ -1,14 +1,6 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 #include <stdint.h>
-
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 200
-
-void graphics_init(void);
-void graphics_clear(uint8_t color);
-void graphics_draw_rect(int x, int y, int w, int h, uint8_t color);
-void graphics_draw_char(int x, int y, char c, uint8_t color);
-void graphics_draw_string(int x, int y, const char* s, uint8_t color);
-
-#endif // GRAPHICS_H
+void put_pixel(int x, int y, uint8_t color);
+void draw_rect(int x, int y, int w, int h, uint8_t color);
+#endif
--- OptrixOS-Kernel/include/keyboard.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-int keyboard_detect(void);
-void keyboard_init(void);
-char keyboard_getchar(void);
-#endif // KEYBOARD_H
