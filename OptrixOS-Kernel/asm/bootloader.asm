c366d070a10104fdb4a78e373663572de9db73db
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-15 20:23:51 -0400

    Enable protected mode in bootloader

--- OptrixOS-Kernel/asm/bootloader.asm
+++ OptrixOS-Kernel/asm/bootloader.asm
@@ -21,8 +21,42 @@ start:
     mov cx, 0x0002    ; cylinder/head/sector = sector 2
     int 0x13
 
-    ; jump to kernel
-    jmp 0x0000:0x1000
+    ; setup basic GDT for protected mode
+    lgdt [gdt_desc]
+
+    ; enable protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    jmp 0x08:protected_mode
+
+; 32-bit protected mode code
+[BITS 32]
+protected_mode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+
+    call dword 0x1000
+.halt:
+    hlt
+    jmp .halt
+
+; GDT setup
+[BITS 16]
+gdt_start:
+    dq 0x0000000000000000
+    dq 0x00cf9a000000ffff ; code segment
+    dq 0x00cf92000000ffff ; data segment
+gdt_end:
+
+gdt_desc:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 BOOT_DRIVE: db 0
 
