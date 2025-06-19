a55911ec1e396fbf35ef2e961fc84dbf843dc702
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-19 17:03:16 -0400

    Switch ISO build to boot image

---
+++ OptrixOS-Kernel/src/root_files.c
@@ -0,0 +1,6 @@
+#include "root_files.h"
+const root_file root_files[] = {
+    {"resources/welcome.txt", "Welcome to OptrixOS!\nEnjoy your stay.\n"},
+    {"resources/logo.txt", "  ____        _   _      ____  _____ \n / __ \\      | | | |    / __ \\|  __ \\\n| |  | |_ __ | |_| |_  | |  | | |__) |\n| |  | | '_ \\| __| __| | |  | |  ___/\n| |__| | | | | |_| |_  | |__| | |    \n \\____/|_| |_|\\__|\\__|  \\____/|_|    \n"},
+};
+const int root_files_count = 2;
