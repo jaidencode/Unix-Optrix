7de87e3769327bff63f857a010dbd8d5de02b258
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-19 15:01:56 -0400

    Revert "Add external resource disk support"

--- OptrixOS-Kernel/include/disk.h
+++ OptrixOS-Kernel/include/disk.h
@@ -2,7 +2,6 @@
 #define DISK_H
 #include <stdint.h>
 void ata_init(void);
-void ata_select(uint16_t base, int slave);
 int ata_detect(void);
 int ata_read_sector(uint32_t lba, void* buffer);
 int ata_write_sector(uint32_t lba, const void* buffer);
