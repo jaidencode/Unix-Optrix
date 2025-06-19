4c833d66f25698554867c1e2cdaec1a4126ec6a7
jaidencode <33370705+jaidencode@users.noreply.github.com>
2025-06-19 17:07:38 -0400

    Fix kernel loading for ISO

---
+++ OptrixOS-Kernel/include/root_files.h
@@ -0,0 +1,6 @@
+#ifndef ROOT_FILES_H
+#define ROOT_FILES_H
+typedef struct { const char* path; const char* data; } root_file;
+extern const int root_files_count;
+extern const root_file root_files[];
+#endif
